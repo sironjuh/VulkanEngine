dfe57960bc5bd82a35fa0c6d06cda95b9faefc80
Juha-Matti Sironen <juha-matti.sironen@outlook.com>
2022-01-09 11:48:43 +0200

    Use vertexbuffers

--- ve_pipeline.cpp
+++ ve_pipeline.cpp
@@ -1,4 +1,5 @@
 #include "ve_pipeline.hpp"
+#include "ve_model.hpp"
 
 #include <cassert>
 #include <fstream>
@@ -78,12 +79,15 @@ namespace ve
     shaderStages[1].pNext = nullptr;
     shaderStages[1].pSpecializationInfo = nullptr;
 
+    auto bindingDescriptions = VEModel::Vertex::getBindingDescriptions();
+    auto attributeDescriptions = VEModel::Vertex::getAttributeDescriptions();
+
     VkPipelineVertexInputStateCreateInfo vertexInputInfo{};
     vertexInputInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO;
-    vertexInputInfo.vertexAttributeDescriptionCount = 0;
-    vertexInputInfo.vertexBindingDescriptionCount = 0;
-    vertexInputInfo.pVertexAttributeDescriptions = nullptr;
-    vertexInputInfo.pVertexBindingDescriptions = nullptr;
+    vertexInputInfo.vertexAttributeDescriptionCount = static_cast<uint32_t>(attributeDescriptions.size());
+    vertexInputInfo.vertexBindingDescriptionCount = static_cast<uint32_t>(bindingDescriptions.size());
+    vertexInputInfo.pVertexAttributeDescriptions = attributeDescriptions.data();
+    vertexInputInfo.pVertexBindingDescriptions = bindingDescriptions.data();
 
     VkPipelineViewportStateCreateInfo viewportInfo{};
     viewportInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO;
--- vulkan_engine.hpp
+++ vulkan_engine.hpp
@@ -4,6 +4,7 @@
 #include "ve_pipeline.hpp"
 #include "ve_swap_chain.hpp"
 #include "ve_window.hpp"
+#include "ve_model.hpp"
 
 #include <memory>
 #include <vector>
@@ -12,29 +13,37 @@ namespace ve
 {
   class VulkanEngine
   {
-    public:
-      static constexpr int WIDTH = 800;
-      static constexpr int HEIGHT = 600;
+  public:
+    static constexpr int WIDTH = 800;
+    static constexpr int HEIGHT = 600;
 
-      VulkanEngine();
-      ~VulkanEngine();
+    VulkanEngine();
+    ~VulkanEngine();
 
-      VulkanEngine(const VulkanEngine&) = delete;
-      VulkanEngine& operator=(const VulkanEngine&) = delete;
+    VulkanEngine(const VulkanEngine &) = delete;
+    VulkanEngine &operator=(const VulkanEngine &) = delete;
 
-      void run();
+    void run();
 
-    private:
-      void createPipelineLayout();
-      void createPipeline();
-      void createCommandBuffers();
-      void drawFrame();
+  private:
+    void sierpinski(
+        std::vector<VEModel::Vertex> &vertices,
+        int depth,
+        glm::vec2 left,
+        glm::vec2 right,
+        glm::vec2 top);
+    void loadModels();
+    void createPipelineLayout();
+    void createPipeline();
+    void createCommandBuffers();
+    void drawFrame();
 
-      VEWindow veWindow{WIDTH, HEIGHT, "Vulkan Engine"};
-      VEDevice veDevice{veWindow};
-      VESwapChain veSwapChain{veDevice, veWindow.getExtent()};
-      std::unique_ptr<VEPipeline> vePipeline;
-      VkPipelineLayout pipelineLayout;
-      std::vector<VkCommandBuffer> commandBuffers;
+    VEWindow veWindow{WIDTH, HEIGHT, "Vulkan Engine"};
+    VEDevice veDevice{veWindow};
+    VESwapChain veSwapChain{veDevice, veWindow.getExtent()};
+    std::unique_ptr<VEPipeline> vePipeline;
+    VkPipelineLayout pipelineLayout;
+    std::vector<VkCommandBuffer> commandBuffers;
+    std::unique_ptr<VEModel> veModel;
   };
 }
