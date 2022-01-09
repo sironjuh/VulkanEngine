e4da5fad257f74f89bc4070b4f119165b79cc72e
Juha-Matti Sironen <juha-matti.sironen@outlook.com>
2022-01-09 11:51:46 +0200

    Missing model class

---
+++ ve_model.cpp
@@ -0,0 +1,69 @@
+#include "ve_model.hpp"
+
+#include <cassert>
+#include <cstring>
+
+namespace ve
+{
+  VEModel::VEModel(VEDevice &device, const std::vector<Vertex> &vertices)
+    : veDevice(device)
+  {
+    createVertexBuffer(vertices);
+  }
+
+  VEModel::~VEModel()
+  {
+    vkDestroyBuffer(veDevice.device(), vertexBuffer, nullptr);
+    vkFreeMemory(veDevice.device(), vertexBufferMemory, nullptr);
+  }
+
+  void VEModel::bind(VkCommandBuffer commandBuffer)
+  {
+    VkBuffer vertexBuffers[] = {vertexBuffer};
+    VkDeviceSize offsets[] = {0};
+    vkCmdBindVertexBuffers(commandBuffer, 0, 1, vertexBuffers, offsets);
+  }
+
+  void VEModel::draw(VkCommandBuffer commandBuffer)
+  {
+    vkCmdDraw(commandBuffer, vertexCount, 1, 0, 0);
+  }
+
+  void VEModel::createVertexBuffer(const std::vector<Vertex> &vertices)
+  {
+    vertexCount = static_cast<uint32_t>(vertices.size());
+    assert(vertexCount >= 3 && "Model must have at least 3 vertices");
+    VkDeviceSize bufferSize = sizeof(vertices[0]) * vertexCount;
+
+    veDevice.createBuffer(
+      bufferSize,
+      VK_BUFFER_USAGE_VERTEX_BUFFER_BIT,
+      VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
+      vertexBuffer,
+      vertexBufferMemory);
+
+    void *data;
+    vkMapMemory(veDevice.device(), vertexBufferMemory, 0, bufferSize, 0, &data);
+    memcpy(data, vertices.data(), static_cast<size_t>(bufferSize));
+    vkUnmapMemory(veDevice.device(), vertexBufferMemory);
+  }
+
+  std::vector<VkVertexInputBindingDescription> VEModel::Vertex::getBindingDescriptions()
+  {
+    std::vector<VkVertexInputBindingDescription> bindingDescriptions(1);
+    bindingDescriptions[0].binding = 0;
+    bindingDescriptions[0].stride = sizeof(Vertex);
+    bindingDescriptions[0].inputRate = VK_VERTEX_INPUT_RATE_VERTEX;
+    return bindingDescriptions;
+  }
+  
+  std::vector<VkVertexInputAttributeDescription> VEModel::Vertex::getAttributeDescriptions()
+  {
+    std::vector<VkVertexInputAttributeDescription> attributeDescriptions(1);
+    attributeDescriptions[0].binding = 0;
+    attributeDescriptions[0].location = 0;
+    attributeDescriptions[0].format = VK_FORMAT_R32G32_SFLOAT;
+    attributeDescriptions[0].offset = 0;
+    return attributeDescriptions;
+  }
+}
---
+++ ve_model.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "ve_device.hpp"
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/glm.hpp>
+
+#include <vector>
+
+namespace ve
+{
+  class VEModel {
+    public:
+      struct Vertex {
+        glm::vec2 position;
+
+        static std::vector<VkVertexInputBindingDescription> getBindingDescriptions();
+        static std::vector<VkVertexInputAttributeDescription> getAttributeDescriptions();
+      };
+
+      VEModel(VEDevice &device, const std::vector<Vertex> &vertices);
+      ~VEModel();
+
+      VEModel(const VEModel &) = delete;
+      void operator=(const VEModel &) = delete;
+
+      void bind(VkCommandBuffer commandBuffer);
+      void draw(VkCommandBuffer commandBuffer);
+
+    private:
+      void createVertexBuffer(const std::vector<Vertex> &vertices);
+
+      VEDevice &veDevice;
+      VkBuffer vertexBuffer;
+      VkDeviceMemory vertexBufferMemory;
+      uint32_t vertexCount;
+  };
+}
