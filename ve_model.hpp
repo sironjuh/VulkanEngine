aaf3d27a6dd9168514bad992f410e6857cd79a0c
Juha-Matti Sironen <sironjuh@users.noreply.github.com>
2022-01-15 23:01:32 +0200

    Pass vertex color data interleaved (#3)

--- ve_model.hpp
+++ ve_model.hpp
@@ -14,6 +14,7 @@ namespace ve
     public:
       struct Vertex {
         glm::vec2 position;
+        glm::vec4 color;
 
         static std::vector<VkVertexInputBindingDescription> getBindingDescriptions();
         static std::vector<VkVertexInputAttributeDescription> getAttributeDescriptions();
